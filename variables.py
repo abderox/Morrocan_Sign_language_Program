f69bec3e2e9c12fb9797d4496ec89ee4351fb734
kaoutar-ou <kaoutar.oubenaddi@gmail.com>
2022-05-23 00:36:30 +0100

    lfs

--- variables.py
+++ variables.py
@@ -7,6 +7,10 @@ IMAGE_SIZE = 50  # We'll be working with 50 * 50 pixel images
 # Minimum confidence percentage i.e allowed for prediction
 THRESHOLD = 25
 
+# MODEL_PATH = "model_test_2.h5"
 MODEL_PATH = "model_test.h5"
 
-LABELS = ['AV','B','T','TH']
+# LABELS = ['A','AIN','B','D','DAD','GH','L','R','S','T','TH','Y']
+LABELS = ['أ', 'ع', 'ب', 'د', 'ض', 'غ', 'ل', 'ر', 'س', 'ت', 'ي']
+# LABELS = ['د','ل','ي']
+# LABELS = ['ب','ت','ث']
